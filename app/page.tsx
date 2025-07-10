f381aab656205db68c980882f3b7876f61efc607
Elitelord <sameeragarwal007@outlook.com>
2025-07-09 21:55:58 -0500

    adding api get request

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
 import Dashboard from "./Dashboard";
-
-export default async function Home() {
-  const data = await fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_4LVjUfRQv74JmUVqmoP2QZ8P7aAqurvHQAMC3QdW&currencies=EUR%2CUSD%2CCAD',{ cache: 'force-cache' }).then((res) =>
-    res.json()
-  )
-  
+import { useState } from "react";
+import CurrencyAPI from '@everapi/currencyapi-js';
+export default function Home() {
+  // const data = await fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_4LVjUfRQv74JmUVqmoP2QZ8P7aAqurvHQAMC3QdW&currencies=EUR%2CUSD%2CCAD',{ cache: 'force-cache' }).then((res) =>
+  //   res.json()
+  // )
+  const client = new CurrencyAPI(process.env.api_key)
+  const [info,setInfo]=useState("");
+client.latest({
+    base_currency: 'USD',
+    currencies: 'EUR'
+}).then((response: string) => {
+    console.log(response)
+    setInfo(response);
+});
   return (
     <main className={styles.main}>
-      <p>{data[0]}</p>
+      <p>{info[1]}</p>
       
     </main>
   );
